108cab681e93255c4d225eefff55ab619d5c39c7
Joanna Tomassoni <jctomassoni@gmail.com>
2019-12-26 11:05:46 -0600

    add app.js and index.js files, connect to 8080 successfully

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// call middleware functions
+app.use(bodyParser.json());
+app.use(cors());
+
+// serve static assets
+const CLIENT_PATH = path.resolve(__dirname, '../client/public');
+
+app.use(express.static(CLIENT_PATH));
+
+module.exports.app = app;
