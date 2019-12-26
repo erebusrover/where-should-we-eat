09d0cafb6660fd2d0c550ac1180b95e8104f45c7
Joanna Tomassoni <jctomassoni@gmail.com>
2019-12-26 11:11:34 -0600

    fix CLIENT_PATH var

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // serve static assets
-const CLIENT_PATH = path.resolve(__dirname, '../client/public');
+const CLIENT_PATH = path.resolve(__dirname, '../public');
 
 app.use(express.static(CLIENT_PATH));
 
