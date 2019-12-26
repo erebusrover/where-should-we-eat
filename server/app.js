eae14d687018cc226846b2430947cc7cd82c97f4
Joanna Tomassoni <jctomassoni@gmail.com>
2019-12-26 14:34:51 -0600

    small mod to app.js to try to serve react app for azure

--- server/app.js
+++ server/app.js
@@ -14,12 +14,14 @@ app.use(cors());
 // serve static assets
 const CLIENT_PATH = path.resolve(__dirname, '../build');
 
-app.use(express.static(CLIENT_PATH));
+// api routers from router.js
+app.use('/api', router);
 
+// serve react app
+app.use(express.static(CLIENT_PATH));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// app.use('/api', router);
 
 module.exports.app = app;
