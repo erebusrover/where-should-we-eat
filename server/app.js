d29d0de2f8c3cc25f511fd7f6ccdee9a45af4851
Joanna Tomassoni <jctomassoni@gmail.com>
2019-12-26 16:34:33 -0600

    test-- change public to build when serving static assets

--- server/app.js
+++ server/app.js
@@ -12,15 +12,15 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // serve static assets
-const CLIENT_PATH = path.resolve(__dirname, '../public');
+const CLIENT_PATH = path.resolve(__dirname, '../build');
 
 // api routers from router.js
-app.use('/api', router);
+// app.use('/api', router);
 
 // serve react app
 app.use(express.static(CLIENT_PATH));
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
