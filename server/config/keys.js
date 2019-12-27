d5237709f064b7e38aa9fb1abf052cacfa197c7b
Joanna Tomassoni <jctomassoni@gmail.com>
2019-12-27 17:53:39 -0600

    add keys.js to gitignore, add yelp api key

--- server/config/keys.js
+++ server/config/keys.js
@@ -5,4 +5,8 @@ module.exports = {
     clientID: '',
     clientSecret: '',
   },
+  yelp: {
+    CLIENT_ID: 'L-JR_3MGN49jN_uzOVHXlA',
+    API_KEY: '0pc0Eb216zMEvkJx4DN7EC7jXpHPshb7wQr9T_0Y8y62T5RbQaTqvui24bXWmNpGjqxAW__ZCLdhMQCAPuP_MyAHi3aj60BCfXPZZnEYPo7O1clo0qMBKPNk7ZgGXnYx',
+  },
 };
