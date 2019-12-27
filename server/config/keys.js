33792d15f7601545e6babf00c7ffcc3cb83c4a81
Joanna Tomassoni <jctomassoni@gmail.com>
2019-12-27 17:54:54 -0600

    add comments to keys.js

--- server/config/keys.js
+++ server/config/keys.js
@@ -10,3 +10,6 @@ module.exports = {
     API_KEY: '0pc0Eb216zMEvkJx4DN7EC7jXpHPshb7wQr9T_0Y8y62T5RbQaTqvui24bXWmNpGjqxAW__ZCLdhMQCAPuP_MyAHi3aj60BCfXPZZnEYPo7O1clo0qMBKPNk7ZgGXnYx',
   },
 };
+
+// Yelp daily API limit: 500
+// visit https://www.yelp.com/developers/v3/manage_app?app_created=True to refresh API key
