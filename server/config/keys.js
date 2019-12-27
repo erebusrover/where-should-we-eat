905cd3367fce26d8b5c72ec7fb9f0476947dea80
Joanna Tomassoni <jctomassoni@gmail.com>
2019-12-27 17:59:47 -0600

    add more comments

--- server/config/keys.js
+++ server/config/keys.js
@@ -11,5 +11,11 @@ module.exports = {
   },
 };
 
-// Yelp daily API limit: 500
+// Yelp API notes
+// daily limit: 500
+// main endpoint: https://api.yelp.com/v3
+// https://www.yelp.com/developers/documentation/v3/get_started
 // visit https://www.yelp.com/developers/v3/manage_app?app_created=True to refresh API key
+
+// thoughts:
+// maybe we want to only show restaurants with 4 or 5 star reviews
