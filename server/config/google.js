68f82bf384384716148fea4899dabd0934c15d42
Joanna Tomassoni <jctomassoni@gmail.com>
2019-12-28 11:36:53 -0600

    install axios and nodejs google maps library

--- server/config/google.js
+++ server/config/google.js
@@ -1,7 +1,20 @@
 const { API_KEY } = require('./keys.js').googleMaps;
 
-// write function to call to google routes api
+// write function(s) to call to google routes api
 
+
+// TODO: use geolocation API to get user's current location
+// From Google docs:
+// The Geolocation API returns a location and accuracy radius based on information
+// about cell towers and WiFi nodes that the mobile client can detect.
+// POST to:
+// https://www.googleapis.com/geolocation/v1/geolocate?key=YOUR_API_KEY
+const userLocation = () => {
+  
+};
+
+
+// Directions API
 // main endpoint: https://maps.googleapis.com/maps/api/directions
 // set origin and destination, follow with api key
 // example request route:
