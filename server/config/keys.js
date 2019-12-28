b19b24c09492fc9276eb83aee08af7a995fcf86b
Joanna Tomassoni <jctomassoni@gmail.com>
2019-12-28 11:33:40 -0600

    delete keys

--- server/config/keys.js
+++ server/config/keys.js
@@ -6,10 +6,10 @@ module.exports = {
     clientSecret: '',
   },
   googleMaps: {
-    API_KEY: 'AIzaSyBzt9ZXHKl4Qh7IYslW3MKBT3ay4lekXaY',
+    API_KEY: '',
   },
   yelp: {
     CLIENT_ID: 'L-JR_3MGN49jN_uzOVHXlA',
-    API_KEY: '0pc0Eb216zMEvkJx4DN7EC7jXpHPshb7wQr9T_0Y8y62T5RbQaTqvui24bXWmNpGjqxAW__ZCLdhMQCAPuP_MyAHi3aj60BCfXPZZnEYPo7O1clo0qMBKPNk7ZgGXnYx',
+    API_KEY: '',
   },
 };
