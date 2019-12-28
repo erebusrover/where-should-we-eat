c3569fe0b418a699b04081135d89dab3d39211a3
Joanna Tomassoni <jctomassoni@gmail.com>
2019-12-28 15:26:55 -0600

    comments and some pseudocode

--- server/config/yelp.js
+++ server/config/yelp.js
@@ -1,23 +1,44 @@
+const axios 
 const { CLIENT_ID, API_KEY } = require('./keys').yelp;
 
 // write function for yelp api calls here
+const getRestaurants = (query) => {
+  // query argument should include the following properties:
+    // user lat/lng: --> retrieved from call to google api
+      // latitude
+      // longitude
+    // radius --> part of user preferences or chosen for each new decision?
+    // categories --> derived from group/user preferences
+    // price --> derived from group/user preferences
+  // so something like this:
+  // query = {
+  //   latitude: user.lat,
+  //   longitude: user.lng,
+  //   radius: 10000,
+  //   categories: 'vegan, italian',
+  //   price: '1, 2',
+  // }
+  // search url should look something like this:
+  // https://api.yelp.com/v3/businesses/search?latitude=29.985996800000002&longitude=-90.08414719999999&radius=40000&categories=vegan&limit=5&sort_by=rating&price=1, 2& Authorization=Bearer API_KEY
+
+  return axios.get(url);
+};
+
+module.exports.getRestaurants = getRestaurants;
 
 // search params:
 // location or lat/lng
+// radius: max distance radius (in meters)
 // categories: a list of comma-delimited categories that we get from user preferences:
 // https://www.yelp.com/developers/documentation/v3/all_category_list
 // limit: 5?
 // sort_by: rating?
-// price: get from user preferences
+// price: get from user preferences. 1, 2, 3, or 4. can a list
+// (i.e. "1, 2, 3" will include restaurants in the $, $$, $$$ ranges)
 
-// Yelp API notes
-// daily limit: 500
-// main endpoint: https://api.yelp.com/v3/businesses/search
+
 // To get detailed information and reviews,
 // use the Business ID returned from main endpoint with
 // /businesses/{id} and /businesses/{id}/reviews endpoints.
 // https://www.yelp.com/developers/documentation/v3/get_started
 // visit https://www.yelp.com/developers/v3/manage_app?app_created=True to refresh API key
-
-// thoughts:
-// maybe we want to only show restaurants with 4 or 5 star reviews
