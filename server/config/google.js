6da5577e97fbce9168a4ae5c8052d622c038b904
Joanna Tomassoni <jctomassoni@gmail.com>
2019-12-28 10:50:13 -0600

    comments in google.js

--- server/config/google.js
+++ server/config/google.js
@@ -1,3 +1,8 @@
 const { API_KEY } = require('./keys.js').googleMaps;
 
 // write function to call to google routes api
+
+// main endpoint: https://maps.googleapis.com/maps/api/directions
+// set origin and destination, follow with api key
+// example request route:
+// https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=YOUR_API_KEY
