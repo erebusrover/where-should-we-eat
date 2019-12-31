f37289fa854f75c980f75451bffc017fea89b34f
Joanna Tomassoni <jctomassoni@gmail.com>
2019-12-31 16:06:19 -0600

    one more minor deployment change

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use('/api', router);
 
 // serve static assets
 const CLIENT_PATH = path.resolve(__dirname, '../build');
-app.use(express.static('build'));
+app.use(express.static(CLIENT_PATH));
 // send users to main index page on all endpoints
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
