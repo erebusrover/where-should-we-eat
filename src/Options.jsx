6fe48c9c48c61f553937d89efd95c32ba2a07200
dotbehrens <dotbehrens@gmail.com>
2020-01-01 13:28:38 -0600

    created option item component

--- src/Options.jsx
+++ src/Options.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import OptionItem from './OptionItem.jsx';
 
 const Options = (props) => {
   const { options } = props;
   return (
       <div>
           <h2>{}</h2>
+          <div>{options.map((option) => (
+                  <OptionItem option={option}/>
+          ))}</div>
           <h2>This has rendered </h2>
       </div>
 
