2357abc7a717f8b9fc534a92fbdf21b257339888
Joanna Tomassoni <jctomassoni@gmail.com>
2020-01-02 14:31:27 -0600

    delete build folder, add to gitignore, comment out google call bind in app.jsx

--- server/app.js
+++ server/app.js
@@ -27,11 +27,11 @@ app.use('/api', router);
 
 
 // serve static assets
-const CLIENT_PATH = path.resolve(__dirname, 'build');
+const CLIENT_PATH = path.resolve(__dirname, '../build');
 app.use(express.static(CLIENT_PATH));
 // send users to main index page on all endpoints
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
 });
 
 // app.get('/', (req, res) => {
