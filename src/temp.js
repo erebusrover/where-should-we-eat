ff82626c4976ae222d8f76a041940f8828c34869
dotbehrens <dotbehrens@gmail.com>
2020-01-02 17:31:27 -0600

    added some test data for choices

--- src/temp.js
+++ src/temp.js
@@ -4,13 +4,13 @@ const response = [
     rating: 8,
     price: '$$',
     phone: '+14152520800',
-    address1: '375 Valencia St',
+    address: '375 Valencia St',
   },
   {
     name: 'Four Barrel Coffee',
     rating: 4,
     price: '$',
     phone: '+14152520800',
-    address1: '375 Valencia St',
+    address: '375 Amadee St',
   },
 ];
