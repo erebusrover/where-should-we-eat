5a8da375fe1f30adc862b7bad458f12a2b95beed
dotbehrens <dotbehrens@gmail.com>
2020-01-02 16:10:58 -0600

    test data

---
+++ src/temp.js
@@ -0,0 +1,16 @@
+const response = [
+  {
+    name: 'Four Barrel Rum',
+    rating: 8,
+    price: '$$',
+    phone: '+14152520800',
+    address1: '375 Valencia St',
+  },
+  {
+    name: 'Four Barrel Coffee',
+    rating: 4,
+    price: '$',
+    phone: '+14152520800',
+    address1: '375 Valencia St',
+  },
+];
