8f7abe0ff5cf41e12378c43179e6181516cfce6a
Joanna Tomassoni <jctomassoni@gmail.com>
2020-01-02 22:13:56 -0600

    add manifest-src to CSP header whitelist

--- server/app.js
+++ server/app.js
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 app.use('*', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Content-Security-Policy', 'script-src "self", manifest-src "self"');
   next();
 });
 app.options('*', cors());
