8b0dd278ff004adf20a57ae918aee3bb4c646fe7
Joanna Tomassoni <jctomassoni@gmail.com>
2020-01-03 15:34:08 -0600

    db connection working?!?

--- server/db/config.js
+++ server/db/config.js
@@ -7,59 +7,54 @@ dotenv.config();
 // ^^^ injects the dotenv package into our project configuration
 // process.env now has the keys and values defined in .env
 
-// const {
-//   DB_HOST,
-//   DB_PORT,
-//   DB_USER,
-//   USER_PASS,
-//   DATABASE,
-// } = process.env;
-
-// Database Connection for Production
+const {
+  DB_USER,
+  DB_PASS,
+  DB_NAME,
+  DB_HOST,
+  DB_PORT,
+  CLOUD_SQL_CONNECTION_NAME,
+} = process.env;
 
 const config = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  user: DB_USER,
+  password: DB_PASS,
+  database: DB_NAME,
+  host: DB_HOST,
+  port: DB_PORT,
+  socketPath: `/cloudsql/${CLOUD_SQL_CONNECTION_NAME}`,
 };
-
-// if (process.env.CLOUD_SQL_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
-//   config.socketPath = `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`;
-//   console.log('connected');
-// }
 
 // const connection = mysql.createConnection(config);
 
+// console.log(CLOUD_SQL_CONNECTION_NAME);
+// // create connection
+// connection.connect((err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(`Connected to database ${DB_NAME}`);
+//   }
+// });
 
-// [START cloud_sql_mysql_mysql_create]
+
 let pool;
-const createPool = async () => {
-  pool = await mysql.createPool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+const createPool = () => {
+  pool = mysql.createPool({
+    user: process.env.DB_USER, // e.g. 'my-db-user'
+    password: process.env.DB_PASS, // e.g. 'my-db-password'
+    database: process.env.DB_NAME, // e.g. 'my-database'
     // If connecting via unix domain socket, specify the path
     socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
     // If connecting via TCP, enter the IP and port instead
-    // host: process.env.DB_HOST,
-    // port: process.env.DB_PORT,
+    // host: 'localhost',
+    // port: 3306,
+
+    //...
   });
 };
-createPool().then(() => {
-  console.log(`connected to ${process.env.DB_NAME}???`);
-});
-
-// const ensureSchema = async () => {
-//   // Wait for tables to be created (if they don't already exist).
-//   await pool.query(
-//     `CREATE TABLE IF NOT EXISTS votes
-//       ( vote_id SERIAL NOT NULL, time_cast timestamp NOT NULL,
-//       candidate CHAR(6) NOT NULL, PRIMARY KEY (vote_id) );`,
-//   );
-// };
-// ensureSchema();
+createPool();
 
 
 // module.exports.connection = connection;
-module.exports.pool = createPool;
+module.exports.pool = pool;
