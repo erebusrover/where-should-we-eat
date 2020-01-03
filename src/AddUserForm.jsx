f221ed2b1b6a02f25d360b93e8d1f4f912269fe9
dotbehrens <dotbehrens@gmail.com>
2020-01-03 14:38:46 -0600

    added select menu

--- src/AddUserForm.jsx
+++ src/AddUserForm.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import { Select } from '@material-ui/core';
 
 const AddUserForm = (props) => {
   const { handleNewGroupMember, handleAddUserToGroup } = props;
   return (
       <div>
+        <Select multiple='true' autoWidth='true' IconComponent='ArrowDropDownIcon'></Select>
           <h1>Add Group Member By Name</h1>
           <input type='text' onChange={handleNewGroupMember}/>
-         <Button onClick={() => { handleAddUserToGroup(); }}>Add Group Member</Button>
+          <Button onClick={() => { handleAddUserToGroup(); }}>Add Group Member</Button>
       </div>
   );
 };
