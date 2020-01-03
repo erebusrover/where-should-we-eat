3d3e3c7c3aca6d0f81c5e2f5b1ec95416059a512
Jeff <jeff.l.watkins2@gmail.com>
2020-01-03 13:17:19 -0600

    Cleaned up redirect route

--- server/router.js
+++ server/router.js
@@ -344,8 +344,7 @@ router.get('/login', passport.authenticate('google', {
 }));
 
 // GET /redirect to reroute back to the app from the google consent screen
-router.get('/', passport.authenticate('google'), (req, res) => {
-  console.log('you hit the redirect route');
+router.get('/login/redirect', passport.authenticate('google'), (req, res) => {
   res.redirect('/');
 });
 
