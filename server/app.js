a2c45b273d6089090f229b9e48419df9d7fc1ec5
Joanna Tomassoni <jctomassoni@gmail.com>
2020-01-03 17:56:33 -0600

    npm install and comment out a line inapp.js

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use('*', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Content-Security-Policy', 'script-src "self", manifest-src "self"');
+  // res.setHeader('Content-Security-Policy', 'script-src "self", manifest-src "self"');
   next();
 });
 
