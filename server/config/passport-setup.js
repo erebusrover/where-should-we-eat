894f7e5f6ba606a9080755585e4bdb8040192871
Jeff <jeff.l.watkins2@gmail.com>
2020-01-03 14:04:45 -0600

    Serializing and deSerializing users

--- server/config/passport-setup.js
+++ server/config/passport-setup.js
@@ -3,6 +3,14 @@ const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
 const { addNewUser, checkDb } = require('../db/helpers');
 
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
 passport.use(
   new GoogleStrategy({
     // options for google strategy
@@ -15,19 +23,16 @@ passport.use(
       .then((currentUser) => {
         console.log(currentUser);
         if (currentUser) {
-          return done(null, currentUser);
+          done(null, currentUser);
         }
         if (!currentUser) {
           addNewUser(profile.displayName, profile.id)
             .then((newUser) => {
               console.log(`new user added: ${newUser}`);
-              return done(null, newUser);
+              done(null, newUser);
             });
         }
       });
   }),
 );
 
-passport.serializeUser((user, next) => {
-  next(null, user.google);
-});
