8e8f41d506ac86eb6fa1c4d00b3e5e92d08b3037
Joanna Tomassoni <jctomassoni@gmail.com>
2020-01-04 23:49:19 -0600

    write GET /choices so restaurants render dynamically on Show Options click

--- server/db/helpers.js
+++ server/db/helpers.js
@@ -108,6 +108,14 @@ const getAllGroupMembers = async (groupName) => {
   return pool.query(sql, [groupName]);
 };
 
+// get all dietary restrictions for users of a given group
+const getAllUserRestrictions = async (groupName) => {
+  const sql = `SELECT restriction from dietaryRestrictions WHERE user_id IN
+                (SELECT user_id FROM user_group WHERE groupp_id IN 
+                  (SELECT groupp_id FROM groupp WHERE groupName = ?))`;
+  return pool.query(sql, [groupName]);
+};
+
 // get all groups that a given user belongs to
 const getAllUserGroups = async (userName) => {
   const sql = `SELECT * FROM groupp WHERE groupp_id IN  
@@ -190,6 +198,7 @@ module.exports = {
   addNewGroup,
   addUserToGroup,
   getAllGroupMembers,
+  getAllUserRestrictions,
   getAllUserGroups,
   getAllActiveUserGroups,
   getAllInactiveUserGroups,
