5fb33c1c9ed772b1ff437a2a7b3e2537254b7baf
Joanna Tomassoni <jctomassoni@gmail.com>
2020-01-05 11:14:40 -0600

    add button to option items

--- src/OptionItem.jsx
+++ src/OptionItem.jsx
@@ -10,18 +10,17 @@ const OptionItem = (props) => {
     phone,
     url,
     is_closed,
-    display_phone,
     lat,
     lng
   } = props;
   return (
       <div>
        <h1>{name}</h1>
+       <button>We're eating here</button>
        <h2>{price}</h2>
        <h2>{rating}</h2>
-       <h2>{phone}</h2>
        <h2>Open? {is_closed}</h2>
-       <h2>Phone: {display_phone}</h2>
+       <h2>Phone: {phone}</h2>
        <img src={image_url}/>
       </div>
   );
