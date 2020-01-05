fb46ce2e721c7dcab150f15c9fc70bb7d07828f5
Joanna Tomassoni <jctomassoni@gmail.com>
2020-01-04 21:15:33 -0600

    add rest id as key to each OptonItem

--- src/Options.jsx
+++ src/Options.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import OptionItem from './OptionItem.jsx';
 import Button from '@material-ui/core/Button';
+import OptionItem from './OptionItem.jsx';
 
 const Options = (props) => {
   const { options, handlePass} = props;
+  const { businesses } = options.data;
+  console.log(businesses);
   return (
       <div>
           <h2>{}</h2>
-          <div>{options.map((option) => (
-                  <OptionItem option={option} className='option'/>
+          <div>{businesses.map((option) => (
+                  <OptionItem key={option.id} option={option} className='option'/>
           ))}</div>
-             <Button variant="contained" color="primary" onClick={() => {handlePass() }}>Pass</Button>
+             <Button variant="contained" color="primary" onClick={() => { handlePass() }}>Pass</Button>
       </div>
 
   );
