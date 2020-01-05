84c0ee31fc9834b90d15f33ff6db227a0481e2a9
Joanna Tomassoni <jctomassoni@gmail.com>
2020-01-05 14:17:21 -0600

    add link to Dialog in group

--- src/Group.jsx
+++ src/Group.jsx
@@ -3,12 +3,13 @@ import Button from '@material-ui/core/Button';
 import PropTypes from 'prop-types';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Link from '@material-ui/core/Link';
 import GroupMember from './GroupMember.jsx';
 
 const Group = (props) => {
   const {
     user, groupName, pricePoint, groupMembers,
-    handleViewChange, userImages, handleGetOptions, 
+    handleViewChange, userImages, handleGetOptions,
     choiceName, choiceLat, choiceLng, randomizer, chooser, showWinner,
     toggleDialog, open,
   } = props;
@@ -27,7 +28,8 @@ const Group = (props) => {
           : <h2></h2>}
         </div>
       <Dialog onBackdropClick={() => { toggleDialog(); }} open={open}>
-        <DialogTitle>{choiceName} has been chosen! Click here for directions.}</DialogTitle>
+        <DialogTitle>{chooser} chose {choiceName}.</DialogTitle>
+        <Link href="www.google.com" target="_blank" rel="noreferrer">Click here for directions.</Link>
       </Dialog>
         <ul>
           {groupMembers.map((groupMember) => <GroupMember userImages={userImages} groupMember={groupMember} />)}
@@ -36,7 +38,6 @@ const Group = (props) => {
         <Button onClick={() => { handleViewChange('removeUserFromGroup'); }}>Remove Group Member</Button>
         <Button onClick={() => { randomizer(); }}>Start Game</Button>
       </div>
-
   );
 };
 
