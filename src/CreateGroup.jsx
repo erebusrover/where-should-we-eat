a22aee2919f4622e36d92a0eca4d573d4c4c582b
Joy <greeniejoy@gmail.com>
2020-01-07 16:30:34 -0600

    added material ui input field

--- src/CreateGroup.jsx
+++ src/CreateGroup.jsx
@@ -5,6 +5,7 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Radio from '@material-ui/core/Radio';
 import { withStyles } from '@material-ui/core/styles';
 import { deepPurple } from '@material-ui/core/colors';
+import { TextField } from '@material-ui/core';
 
 /**
  * This is rendered whn a user clicks on the Create Group button in the header.
@@ -29,21 +30,21 @@ const CreateGroup = (props) => {
     await handleViewChange('home');
   };
   return (
-      <div>
-          <br />
-          <form>
-          <h1>Group Name</h1>
-          <input type='text' onChange={handleNewGroupName}></input>
-          </form>
-             <h3> Select a Price </h3>
-        <RadioGroup aria-label="dietary restriction" name="dietary restriction">
-          <FormControlLabel value="$" control={<PurpleRadio />} label="$" onClick={() => handleNewGroupPricePoint('$')} />
-          <FormControlLabel value="$$" control={<PurpleRadio />} label="$$" onClick={() => handleNewGroupPricePoint('$$')} />
-          <FormControlLabel value="$$$" control={<PurpleRadio />} label="$$$" onClick={() => handleNewGroupPricePoint('$$$')} />
-          <FormControlLabel value="$$$$" control={<PurpleRadio />} label="$$$$" onClick={() => handleNewGroupPricePoint('$$$$')} />
-        </RadioGroup>
-        <Button variant="outlined" style={{ background: '#9900cc', color: 'white' }} value='profile' onClick={() => clickFunction()}>Submit</Button>
-      </div>
+    <div>
+      <br />
+      <form>
+        <h1>Group Name</h1>
+        <TextField id="outlined-basic" label="Group Name" variant="outlined" onChange={handleNewGroupName} />
+      </form>
+      <h3> Select a Price </h3>
+      <RadioGroup aria-label="dietary restriction" name="dietary restriction">
+        <FormControlLabel value="$" control={<PurpleRadio />} label="$" onClick={() => handleNewGroupPricePoint('$')} />
+        <FormControlLabel value="$$" control={<PurpleRadio />} label="$$" onClick={() => handleNewGroupPricePoint('$$')} />
+        <FormControlLabel value="$$$" control={<PurpleRadio />} label="$$$" onClick={() => handleNewGroupPricePoint('$$$')} />
+        <FormControlLabel value="$$$$" control={<PurpleRadio />} label="$$$$" onClick={() => handleNewGroupPricePoint('$$$$')} />
+      </RadioGroup>
+      <Button variant="outlined" style={{ background: '#9900cc', color: 'white' }} value='profile' onClick={() => clickFunction()}>Submit</Button>
+    </div>
 
   );
 };
