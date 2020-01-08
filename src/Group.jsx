9f21a788cbb60db93baddeec2fa0dfc143110405
Marc West <marcdwest@gmail.com>
2020-01-08 15:09:34 -0600

    add veto button

--- src/Group.jsx
+++ src/Group.jsx
@@ -1,179 +1,122 @@
-import React from 'react';
-import {
-  Container,
-  Button,
-  Dialog,
-  DialogTitle,
-  Link,
-} from '@material-ui/core';
-import GroupMember from './GroupMember.jsx';
-import { TextField } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-    '& > *': {
-      margin: theme.spacing(1),
-      width: theme.spacing(16),
-      height: theme.spacing(16),
-    },
-  },
-  // paper: {
-  //   padding: theme.spacing(2),
-  //   textAlign: 'center',
-  //   color: theme.palette.text.secondary,
-  // },
-}));
-
-/**
- * This is the component that is rendered when a user clicks on one of their groups
- * from their home page.
- *
- * A user can click Start Game to initiate a decision making 'game,'
- * If the user is randomly chosen as the decision maker, they will be presented with an
- * input form and a button, Show Options. They can optionally put something in the input form
- * to add a parameter to the Yelp API query that will result from them clicking the Show Options button.
- * Clicking the Show Options button renders the Options view/component, which will have a list of five
- * restaurant choices for them to choose.
- *
- * We were attempting to make this stateful in order
- * to implement occasional rerendering of state based on changes to the database,
- * but that is not a currently active feature.
- */
-class Group extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      winner: '',
-    };
-  }
-
-  render() {
-    const {
-      history,
-      user,
-      groupName,
-      pricePoint,
-      members,
-      handleViewChange,
-      userImages,
-      handleGetOptions,
-      choiceName,
-      choiceAddress,
-      randomizer,
-      chooser,
-      showWinner,
-      vetoRandomizer,
-      veto,
-      showVeto,
-      toggleDialog,
-      directionsPopup,
-      handleCategoriesInput,
-    } = this.props;
-
-    const mapsUrl = `https://www.google.com/maps/dir/?api=1&destination=${choiceName} ${choiceAddress}`;
-
-    return (
-      <Container maxWidth="sm">
-        <h1>{groupName}</h1>
-        <h2>Price point: {pricePoint}</h2>
+<Container>
+  <h2 style={{ color: '#d454ff' }}>{groupName}</h2>
+  <h3 style={{ color: '#d454ff' }}>price point: {pricePoint}</h3>
+  <br />
+  <div>
+    {' '}
+    {showWinner === true ? (
+      <div>
+        <h3>{chooser} is the lucky decision maker</h3>
         <br />
         <div>
           {' '}
-          {showWinner === true ? (
+          {chooser && (
             <div>
-              <h1>{chooser} is the lucky Decision Maker!</h1>
-              <br />
-              <h1>
-                {veto} may veto {chooser}'s decision!
-              </h1>
-            </div>
-          ) : (
-            <div>
-              <h2>click 'start game' to generate the group's decision maker</h2>
-              <h2>click 'allow vetoer' to generate a random vetoer</h2>
-            </div>
-          )}
-        </div>
-        <div>
-          {' '}
-          {user === chooser ? (
-            <div>
-              <input
+              <Input
                 id="categories"
                 type="text"
                 onChange={handleCategoriesInput}
-              />
+              />{' '}
               <Button
                 style={{ background: '#9900cc', color: 'white' }}
                 onClick={() => {
                   handleGetOptions();
                 }}
               >
-                Show Options
+                {' '}
+                show options
               </Button>
             </div>
-          ) : (
-            <h2></h2>
           )}
         </div>
-        <Dialog
-          onBackdropClick={() => {
-            toggleDialog('directionsPopup');
-          }}
-          open={directionsPopup}
-        >
-          <DialogTitle>
-            {chooser} chose {choiceName}.
-          </DialogTitle>
-          <Link href={mapsUrl} target="_blank" rel="noreferrer">
-            Click here for directions.
-          </Link>
-        </Dialog>
-        <ul>
-          {members.map(groupMember => (
-            <GroupMember userImages={userImages} groupMember={groupMember} />
-          ))}
-        </ul>
-        <Button
-          style={{ background: '#9900cc', color: 'white' }}
-          onClick={() => {
-            handleViewChange('addUserToGroup');
-          }}
-        >
-          Add Group Member
-        </Button>
-        <br />
-        <Button
-          style={{ background: '#9900cc', color: 'white' }}
-          onClick={() => {
-            randomizer();
-          }}
-        >
-          Start Game
-        </Button>
-        <br />
-        <Button
-          style={{ background: '#9900cc', color: 'white' }}
-          onClick={() => {
-            vetoRandomizer();
-          }}
-        >
-          Allow Vetoer
-        </Button>
-        <div className={useStyles.root}>
-          <Grid container spacing={3}>
-            <Grid item xs={6} sm={6}>
-              <Paper className={useStyles.paper}>History: {history}</Paper>
-            </Grid>
-          </Grid>
+        <div>
+          {' '}
+          {showVeto && (
+            <div>
+              <h3>
+                {veto} may veto {chooser}'s decision
+              </h3>{' '}
+              <Button
+                style={{ background: '#FF0000', color: 'white' }}
+                onClick={() => {
+                  vetoChoice();
+                }}
+              >
+                {' '}
+                Veto
+              </Button>
+            </div>
+          )}
         </div>
-      </Container>
-    );
-  }
-}
-
-export default Group;
+      </div>
+    ) : (
+      <div>
+        <h3>click 'start game' to generate the group's decision maker</h3>
+        <h3>click 'allow vetoer' to generate a random vetoer</h3>
+      </div>
+    )}
+  </div>
+  <br />
+  <Dialog
+    onBackdropClick={() => {
+      toggleDialog('directionsPopup');
+    }}
+    open={directionsPopup}
+  >
+    <DialogTitle>
+      {chooser} chose {choiceName}.
+    </DialogTitle>
+    <Link href={mapsUrl} target="_blank" rel="noreferrer">
+      click here for directions
+    </Link>
+  </Dialog>
+  <div>
+    <ul>
+      {members.map(groupMember => (
+        <GroupMember userImages={userImages} groupMember={groupMember} />
+      ))}
+    </ul>
+  </div>
+  <br />
+  <div>
+    <Button
+      style={{ background: '#9900cc', color: 'white' }}
+      onClick={() => {
+        randomizer();
+      }}
+    >
+      start game
+    </Button>
+    {'  '}
+  </div>
+  <br />
+  <div>
+    <Button
+      style={{ background: '#9900cc', color: 'white' }}
+      onClick={() => {
+        vetoRandomizer();
+      }}
+    >
+      allow vetoer
+    </Button>
+    {'  '}
+  </div>
+  <br />
+  <div>
+    <Button
+      style={{ background: '#d454ff', color: 'white' }}
+      onClick={() => {
+        handleViewChange('addUserToGroup');
+      }}
+    >
+      add group member
+    </Button>{' '}
+    <div className={useStyles.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={6} sm={6}>
+          <Paper className={useStyles.paper}>history: {history}</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  </div>
+</Container>;
