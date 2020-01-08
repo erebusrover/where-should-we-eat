643fa9f00865e0de8bc7955b9989bb1d030468f4
Marc West <marcdwest@gmail.com>
2020-01-07 19:43:26 -0600

    Container

--- src/Group.jsx
+++ src/Group.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Link from '@material-ui/core/Link';
+import {
+  Container,
+  Button,
+  Dialog,
+  DialogTitle,
+  Link,
+} from '@material-ui/core';
 import GroupMember from './GroupMember.jsx';
 
 /**
@@ -15,7 +18,7 @@ import GroupMember from './GroupMember.jsx';
  * to add a parameter to the Yelp API query that will result from them clicking the Show Options button.
  * Clicking the Show Options button renders the Options view/component, which will have a list of five
  * restaurant choices for them to choose.
- * 
+ *
  * We were attempting to make this stateful in order
  * to implement occasional rerendering of state based on changes to the database,
  * but that is not a currently active feature.
@@ -30,40 +33,104 @@ class Group extends React.Component {
 
   render() {
     const {
-      user, groupName, pricePoint, groupMembers,
-      handleViewChange, userImages, handleGetOptions,
-      choiceName, choiceAddress, randomizer, chooser, showWinner,
-      toggleDialog, directionsPopup, handleCategoriesInput,
+      user,
+      groupName,
+      pricePoint,
+      groupMembers,
+      handleViewChange,
+      userImages,
+      handleGetOptions,
+      choiceName,
+      choiceAddress,
+      randomizer,
+      chooser,
+      showWinner,
+      veto,
+      showVeto,
+      toggleDialog,
+      directionsPopup,
+      handleCategoriesInput,
     } = this.props;
     const mapsUrl = `https://www.google.com/maps/dir/?api=1&destination=${choiceName} ${choiceAddress}`;
     return (
-      <div>
+      <Container maxWidth="sm">
         <h1>{groupName}</h1>
         <h2>Price point: {pricePoint}</h2>
         <br />
-        <div> {showWinner === true
-          ? <h1>{chooser} is the lucky Decision Maker</h1>
-          : <div>
-              <h2>Click Start Game to choose the Decision Maker</h2>
-            </div>}
-        </div>
-        <div> {user === chooser
-          ? <div>
-              <input id='categories' type='text' onChange={handleCategoriesInput} />
-              <Button style={{ background: '#9900cc', color: 'white' }} onClick={() => { handleGetOptions(); }}>Show Options</Button>
+        <div>
+          {' '}
+          {showWinner === true ? (
+            <div>
+              <h1>{chooser} is the lucky Decision Maker!</h1>
+              <br />
+              <h1>
+                {veto} may veto {chooser}'s decision!
+              </h1>
             </div>
-          : <h2></h2>}
+          ) : (
+            <div>
+              <h2>Click Start Game to choose the Decision Maker</h2>
+            </div>
+          )}
         </div>
-      <Dialog onBackdropClick={() => { toggleDialog('directionsPopup'); }} open={directionsPopup}>
-          <DialogTitle>{chooser} chose {choiceName}.</DialogTitle>
-          <Link href={mapsUrl} target="_blank" rel="noreferrer">Click here for directions.</Link>
+        <div>
+          {' '}
+          {user === chooser ? (
+            <div>
+              <input
+                id="categories"
+                type="text"
+                onChange={handleCategoriesInput}
+              />
+              <Button
+                style={{ background: '#9900cc', color: 'white' }}
+                onClick={() => {
+                  handleGetOptions();
+                }}
+              >
+                Show Options
+              </Button>
+            </div>
+          ) : (
+            <h2></h2>
+          )}
+        </div>
+        <Dialog
+          onBackdropClick={() => {
+            toggleDialog('directionsPopup');
+          }}
+          open={directionsPopup}
+        >
+          <DialogTitle>
+            {chooser} chose {choiceName}.
+          </DialogTitle>
+          <Link href={mapsUrl} target="_blank" rel="noreferrer">
+            Click here for directions.
+          </Link>
         </Dialog>
         <ul>
-          {groupMembers.map((groupMember) => <GroupMember userImages={userImages} groupMember={groupMember} />)}
+          {groupMembers.map(groupMember => (
+            <GroupMember userImages={userImages} groupMember={groupMember} />
+          ))}
         </ul>
-        <Button style={{ background: '#9900cc', color: 'white' }} onClick={() => { handleViewChange('addUserToGroup'); }}>Add Group Member</Button>
-        <Button style={{ background: '#9900cc', color: 'white' }} onClick={() => { randomizer(); }}>Start Game</Button>
-      </div>
+        <Button
+          style={{ background: '#9900cc', color: 'white' }}
+          onClick={() => {
+            handleViewChange('addUserToGroup');
+          }}
+        >
+          Add Group Member
+        </Button>
+        <br />
+        <Button
+          style={{ background: '#9900cc', color: 'white' }}
+          onClick={() => {
+            randomizer();
+          }}
+        >
+          Start Game
+        </Button>
+      </Container>
     );
   }
 }
