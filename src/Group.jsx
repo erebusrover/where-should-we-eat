dfeb70503e256b119c8f92d07a1229e4461ba98a
Joy <greeniejoy@gmail.com>
2020-01-08 12:07:34 -0600

    mapped over history array to render places

--- src/Group.jsx
+++ src/Group.jsx
@@ -79,7 +79,7 @@ class Group extends React.Component {
 
     return (
       <Container>
-        <h2 style={{ color: '#d454ff'}}>{groupName}</h2>
+        <h2 style={{ color: '#d454ff' }}>{groupName}</h2>
         <h3 style={{ color: '#d454ff' }}>price point: {pricePoint}</h3>
         <br />
         <div>
@@ -88,39 +88,39 @@ class Group extends React.Component {
             <div>
               <h3>{chooser} is the lucky decision maker</h3>
               <br />
-        <div>
-          {' '}
-          {chooser ? (
-            <div>
-              <Input
-                id="categories"
-                type="text"
-                onChange={handleCategoriesInput}
-              />{' '}
-              <Button
-                style={{ background: '#9900cc', color: 'white' }}
-                onClick={() => {
-                  handleGetOptions();
-                }}
-              >{' '}
-                show options
+              <div>
+                {' '}
+                {chooser ? (
+                  <div>
+                    <Input
+                      id="categories"
+                      type="text"
+                      onChange={handleCategoriesInput}
+                    />{' '}
+                    <Button
+                      style={{ background: '#9900cc', color: 'white' }}
+                      onClick={() => {
+                        handleGetOptions();
+                      }}
+                    >{' '}
+                      show options
               </Button>
-            </div>
-          ) : (
-            <h2></h2>
-          )}
-        </div>
+                  </div>
+                ) : (
+                    <h2></h2>
+                  )}
+              </div>
               <h3>
                 {veto} may veto {chooser}'s decision
                 {/* veto options go here */}
               </h3>
             </div>
           ) : (
-            <div>
-              <h3>click 'start game' to generate the group's decision maker</h3>
-              <h3>click 'allow vetoer' to generate a random vetoer</h3>
-            </div>
-          )}
+              <div>
+                <h3>click 'start game' to generate the group's decision maker</h3>
+                <h3>click 'allow vetoer' to generate a random vetoer</h3>
+              </div>
+            )}
         </div>
         <div></div>
         <br />
@@ -138,51 +138,58 @@ class Group extends React.Component {
           </Link>
         </Dialog>
         <div>
-        <ul>
-          {members.map(groupMember => (
-            <GroupMember userImages={userImages} groupMember={groupMember} />
-          ))}
-        </ul>
+          <ul>
+            {members.map(groupMember => (
+              <GroupMember userImages={userImages} groupMember={groupMember} />
+            ))}
+          </ul>
         </div>
         <br />
         <div>
-        <Button
-          style={{ background: '#9900cc', color: 'white' }}
-          onClick={() => {
-            randomizer();
-          }}
-        >
-          start game
+          <Button
+            style={{ background: '#9900cc', color: 'white' }}
+            onClick={() => {
+              randomizer();
+            }}
+          >
+            start game
         </Button>{'  '}
         </div>
         <br />
         <div>
-        <Button
-          style={{ background: '#9900cc', color: 'white' }}
-          onClick={() => {
-            vetoRandomizer();
-          }}
-        >
-          allow vetoer
+          <Button
+            style={{ background: '#9900cc', color: 'white' }}
+            onClick={() => {
+              vetoRandomizer();
+            }}
+          >
+            allow vetoer
         </Button>{'  '}
         </div>
-        <br /> 
+        <br />
         <div>
-        <Button
+          <Button
             style={{ background: '#d454ff', color: 'white' }}
-          onClick={() => {
-            handleViewChange('addUserToGroup');
-          }}
-        >
-          add group member
+            onClick={() => {
+              handleViewChange('addUserToGroup');
+            }}
+          >
+            add group member
         </Button>{' '}
-        <div className={useStyles.root}>
-          <Grid container spacing={3}>
-            <Grid item xs={6} sm={6}>
-              <Paper className={useStyles.paper}>History: {history}</Paper>
+          <div className={useStyles.root}>
+            <Grid container spacing={3}>
+              <Grid item xs={6} sm={6}>
+                <h3>History</h3>
+                {history.map(place =>
+
+                  <Paper className={useStyles.paper}>
+                    {place} <br />
+                  </Paper>
+                )}
+
+              </Grid>
             </Grid>
-          </Grid>
-        </div>
+          </div>
         </div>
       </Container>
     );
