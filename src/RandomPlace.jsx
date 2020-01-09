4bf3d8d322088c3bdf0c8fff1b73878e3c834e31
Joy <greeniejoy@gmail.com>
2020-01-09 11:11:07 -0600

    made components to render randomized option

---
+++ src/RandomPlace.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import OptionItem from './OptionItem.jsx';
+
+/**
+ * Renders five options from a Yelp API call for
+ * places to eat based on the given parameters (either dietary restrictions from the database
+ * or a user string in the Input form in the Group component).
+ *
+ */
+
+const RandomPlace = (props) => {
+  const { options, handlePass, handleChooseOption } = props;
+  return (
+    <div>
+      <h2>{}</h2>
+      <div>{options.map((option) => (
+        <OptionItem
+          key={option.id}
+          id={option.id}
+          name={option.name}
+          address={option.location.address1}
+          city={option.location.city}
+          state={option.location.state}
+          zipCode={option.location.zip_code}
+          price={option.price}
+          rating={option.rating}
+          imageUrl={option.image_url}
+          phone={option.display_phone}
+          is_closed={option.is_closed}
+          url={option.url}
+          lat={option.coordinates.latitude}
+          lng={option.coordinates.longitude}
+          className='option'
+          handleChooseOption={handleChooseOption} />
+      ))}</div>
+    </div>
+  );
+};
+
+export default RandomPlace;
